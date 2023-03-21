fefa1add4f147ffc897100e13a2ebad83381d5af
merzaad <merzaad@gmail.com>
2023-03-21 06:04:18 +0330

    signal, effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import type { Component } from 'solid-js';
+import { Component, createEffect } from 'solid-js'
+import { createSignal } from 'solid-js'
 
 const App: Component = () => {
+  const [state, setState] = createSignal(0)
+  const onIncrease = () => setState((state) => state + 1)
+  createEffect(() => {
+    console.log(state())
+  })
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
-};
+    <div class='flex flex-col justify-center items-center h-screen gap-8'>
+      <p class='text-4xl text-teal-700 text-center'>{state}</p>
+      <button
+        class='p-2 bg-teal-600 rounded-lg hover:scale-125 duration-200'
+        type='button'
+        onClick={onIncrease}
+      >
+        +
+      </button>
+    </div>
+  )
+}
 
-export default App;
+export default App
